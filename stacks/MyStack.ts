5a15736092887264bcce7f895c9f2d8f10a89629
Stephen McElhose <smcelhose.aws@gmail.com>
2023-11-29 20:16:03 -0700

    first commit

---
+++ stacks/MyStack.ts
@@ -0,0 +1,25 @@
+import { StackContext, Api, EventBus } from "sst/constructs";
+
+export function API({ stack }: StackContext) {
+  const bus = new EventBus(stack, "bus", {
+    defaults: {
+      retries: 10,
+    },
+  });
+
+  const api = new Api(stack, "api", {
+    defaults: {
+      function: {
+        bind: [bus],
+      },
+    },
+    routes: {
+      "GET /": "packages/functions/src/lambda.handler",
+    },
+  });
+
+
+  stack.addOutputs({
+    ApiEndpoint: api.url,
+  });
+}
